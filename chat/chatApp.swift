10ef640992686a6ed406ad6ca7eff5503023d710
Imran Abdullah <nagatoyahiko856@gmail.com>
2023-08-26 11:41:33 +0300

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Imran Abdullah on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
